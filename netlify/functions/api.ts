a4b2b644d132e947be5f93cfdde1efee38a0d7fa
Joshua Blew <joshua@normalfinance.io>
2023-12-13 13:37:13 -0600

    sf

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,15 +1,15 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
-import { ThirdwebAuth } from "@thirdweb-dev/auth/express";
-import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
+// import { ThirdwebAuth } from "@thirdweb-dev/auth/express";
+// import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
 
-const app = express();
+const api = express();
 const router = Router();
 
-const { authRouter, authMiddleware, getUser } = ThirdwebAuth({
-  domain: process.env.THIRDWEB_AUTH_DOMAIN || "",
-  wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
-});
+// const { authRouter, authMiddleware, getUser } = ThirdwebAuth({
+//   domain: process.env.THIRDWEB_AUTH_DOMAIN || "",
+//   wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
+// });
 
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
@@ -19,5 +19,5 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 // // Add the auth middleware to the rest of our app to allow user authentication on other endpoints
 // router.use(authMiddleware);
 
-app.use("/api/", router);
-export const handler = serverless(app);
+api.use("/api/", router);
+export const handler = serverless(api);
