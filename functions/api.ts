e6b8529581e9b4207c7000c4e0b0baceb2c38024
Joshua Blew <joshua@normalfinance.io>
2023-12-13 13:30:15 -0600

    fixes

--- functions/api.ts
+++ functions/api.ts
@@ -4,19 +4,20 @@ import { ThirdwebAuth } from "@thirdweb-dev/auth/express";
 import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
 
 const app = express();
+const router = Router();
 
 const { authRouter, authMiddleware, getUser } = ThirdwebAuth({
   domain: process.env.THIRDWEB_AUTH_DOMAIN || "",
   wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
 });
 
-const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 // Add the auth router to our app to set up the /auth/* endpoints
-app.use("/auth", authRouter);
+router.use("/auth", authRouter);
 
 // Add the auth middleware to the rest of our app to allow user authentication on other endpoints
-app.use(authMiddleware);
+router.use(authMiddleware);
 
+app.use("/.netlify/functions/api", router);
 export const handler = serverless(app);
