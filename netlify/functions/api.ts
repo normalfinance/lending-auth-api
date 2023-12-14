923336ea024ed0ccb70ef8cbe7e8bc580c844f53
Joshua Blew <joshua@normalfinance.io>
2023-12-14 12:29:47 -0600

    remove hardcoded values and add dynamic CORS origin

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -7,27 +7,17 @@ import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
 const api = express();
 const router = Router();
 
-var whitelist = ['http://localhost', 'localhost', 'http://localhost:3006', 'localhost:3006', 'http://localhost:3006/']
-var corsOptions = {
+const corsOptions = {
   credentials: true,
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
+  origin: process.env.CORS_ORIGIN,
+};
 
 api.use(cors(corsOptions));
 router.use(cors(corsOptions));
 
 const { authRouter, authMiddleware, getUser } = ThirdwebAuth({
-  domain: "localhost:3006",
-  wallet: new PrivateKeyWallet(
-    "12632ee39b5e32052f9b5d453d8b5f456da6dbc96c90a47e75a0ac7e1fcb8e88"
-  ),
+  domain: process.env.THIRDWEB_AUTH_DOMAIN || "",
+  wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
 });
 
 router.get("/hello", (req, res) => res.send("Hello World!"));
